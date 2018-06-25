e8cdb7712b2d5c72a9e7a7cca3153d68a490e454
Graywd <k.gray.jonescn@gmail.com>
2018-06-25 11:27:40 +0800

    create realtimefunc.py
    add realtimefunc.py

---
+++ realtimefunc.py
@@ -0,0 +1,54 @@
+# -*- coding: iso-8859-1 -*-
+import sys
+import linecache
+import re
+from inspect import getsource, getfile
+
+# A decorator is used to update a function at runtime.
+
+DecoratorName = 'realtimefunc'
+
+PY3 = sys.version_info >= (3,)
+if PY3:
+    basestring_type = str
+else:
+    basestring_type = basestring  # noqa
+
+def _exec_in(code, glob, loc=None):
+    # type: (Any, Dict[str, Any], Optional[Mapping[str, Any]]) -> Any
+    if isinstance(code, basestring_type):
+        # exec(string) inherits the caller's future imports; compile
+        # the string first to prevent that.
+        code = compile(code, '<string>', 'exec', dont_inherit=True)
+    exec(code, glob, loc)
+
+def _handle_real_time_func_code(func, split='\n'):
+    code = getsource(func)
+    i_indent = 0
+    i_decorator = 0
+    code_lines = code.split(split)
+    func_pat = re.compile(r'^\s*def\s+'+func.__name__)
+    for i, line in enumerate(code_lines):
+        if "@"+DecoratorName in line:
+            i_decorator = i
+
+        if  func_pat.match(line):
+            i_indent = line.index("def")
+            break
+    # rm realtimefunc decorator
+    code_lines.pop(i_decorator)
+    # code indentation
+    code_lines = [line[i_indent:] for line in code_lines]
+    code = split.join(code_lines)
+    return code
+
+def realtimefunc(func):
+    def wrapper(*args, **kwargs):
+        filename = getfile(func)
+        # inspect use linecache to do file cache, so do checkcache first
+        linecache.checkcache(filename)
+        code_str = _handle_real_time_func_code(func)
+        _exec_in(code_str,func.__globals__, func.__globals__)
+        # A return expected when is work, if not yield instead.
+        return func.__globals__[func.__name__](*args, **kwargs)
+    return wrapper
